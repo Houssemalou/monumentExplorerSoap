b69bfbeb8958025d6deb2d328f83c01e00f83372
aloui_houssem <aloui9861@gmail.com>
2024-10-28 09:30:22 +0100

    initiate project

---
+++ src/main/java/com/enicarthage/monumentExplorer/MonumentExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.enicarthage.monumentExplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonumentExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonumentExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enicarthage/monumentExplorer/MonumentExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enicarthage.monumentExplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonumentExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
